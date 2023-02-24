c08687760f41ff8823365a590c464dc9b3121634
Szitha Szabolcs <szitha.szabolcs@gmail.com>
2023-02-24 16:55:41 +0100

    -Added gloabl matrix for directional navigation

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 
 
 public class Main {
+
+    /*
+    *  0,1,2,
+    *  3,x,4,
+    *  5,6,7
+    * */
+    public static final int[][] directions = {
+            {-1,-1}, {0,-1}, {1,-1},
+            {-1,0} ,{1,0},
+            {1,1}, {0,1}, {-1,1}
+    };
+
+
     public static void main(String[] args) {
         Garden myGarden = new Garden();
         LawnMower LawnMower = new LawnMower();

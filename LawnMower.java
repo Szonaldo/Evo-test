3b9cd26abb1beddd62623062d95031e1cd2291f5
Szitha Szabolcs <szitha.szabolcs@gmail.com>
2023-02-24 23:18:42 +0100

    Added LawnMower move method Added LawnMower attribute gardenLayout Added LawnMower attribute to track possible move options

--- LawnMower.java
+++ LawnMower.java
@@ -1,14 +1,45 @@
+import java.util.ArrayList;
+
 public class LawnMower {
     private int[] myPosition;
+    private char[][] gardenLayout;
+    private ArrayList<int[]> moveOptions;
 
-    LawnMower(){
-        myPosition = new int[] {0,0};
+    //methods
+    public void move(){
+        int i = 0;
+        for (int[] direction : Main.directions) {
+            int x = direction[0]+myPosition[0];
+            int y = direction[1]+myPosition[1];
+            if (x < gardenLayout.length  &&  x > 0  &&  y < gardenLayout[0].length  &&  y >0
+            ){
+                moveOptions.add(new int[] {x,y});
+            }
+        }
     }
 
+    //constructors
+    public LawnMower(){
+        myPosition = new int[] {0,0};
+        gardenLayout = new char[][]{
+                {'g', 'g', 'g', 'g', 'g'},
+                {'g', 'g', 'g', 'g', 'g'},
+                {'g', 'g', 'g', 'g', 'g'},
+                {'g', 'g', 'g', 'g', 'g'},
+                {'g', 'g', 'g', 'g', 'g'}
+        };
+        moveOptions = null;
+    }
+
+    public LawnMower(int x, int y){
+        myPosition = new int[] {x,y};
+    }
+
+    //getters
     public int[] getMyPosition() {
         return myPosition;
     }
-
+    //setters
     public void setMyPosition(int[] myPosition) {
         this.myPosition = myPosition;
     }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-
-
+import java.io.*;
+//TODO: enum for garden cases [clean,road,obstacle, mixed]
 public class Main {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED = "\u001B[31m";
     /*
     *  0,1,2,
     *  3,x,4,
@@ -17,5 +21,7 @@ public class Main {
     public static void main(String[] args) {
         Garden myGarden = new Garden();
         LawnMower LawnMower = new LawnMower();
+        myGarden.printLayout();
+        System.out.println("test");
     }
 }
